3930b2a0e61dcfe8dfa2960c38769c56c7541268
iiriis <avijitdasxp@gmail.com>
2023-12-13 22:04:17 +0530

    added set baud and timeouts

--- serialPort.c
+++ serialPort.c
@@ -1,45 +1,8 @@
 
 #include <stdio.h>
-#include <errno.h>
-#include <stdint.h>
-#include <windows.h>
+#include <serialPort.h>
 
 
-#define FILE_NO_SHARED_ACCESS   0
-#define FILE_RW_MODE            (FILE_GENERIC_READ | FILE_GENERIC_WRITE)
-#define MAX_ERR_CODE_LEN        64u
-
-/* The Serial Port Structure to store the fields of the serial COMM */
-typedef struct {
-    HANDLE handle;          /* Handle to store the serial port as a file handle */
-    const char *name;       /* name of the serial COM port */
-    boolean isOpen;         /* Boolean value to indicate the port open status */
-    uint64_t baud;          /* Baud rate of the serial COM port */
-    uint32_t readTimeout;   /* read timeout in ms */
-    uint32_t writeTimeout;  /* write timeout in ms */
-}serial_port_t;
-
-/* error enum to store the error types */
-typedef enum {
-    SERIAL_ERR_OK,
-    SERIAL_ERR_OPEN,
-    SERIAL_ERR_CLOSE,
-    SERIAL_ERR_READ_UNKNOWN,
-    SERIAL_ERR_READ_SIZE_MISMATCH,
-    SERIAL_ERR_WRITE_UNKNOWN,
-    SERIAL_ERR_WRITE_SIZE_MISMATCH,
-}serial_port_err_t;
-
-
-/** 
- * Objective: To get the system error code and 
- * print it's text format for human readability.
- * 
- * Parameters: NONE
- * 
- * Returns: NULL
-*/
-void printError();
 
 void printError()
 {
@@ -53,22 +16,53 @@ void printError()
     printf("\x1b[31m%s\x1b[0m\a\n", buf);
 }
 
-/** 
- * Objective: To open the serial port and initialise the 
- * the serial port handle.
- * 
- * Parameters: 
- * serial_port_t* port: Pointer to the serial port structure.
- * const char* name: String containing the name of the serial port.
- * uint64_t baud: Baudrate of the serial port.
- * uint32_t readTimeout: timeout in ms before read action returns.
- * uint32_t writeTimeout: timeout in ms before write action returns.
- *  
- * Returns: serial_port_err_t
- * SERIAL_ERR_OK: on successful poening the serial port.
- * SERIAL_ERR_OPEN: could not open serial port.
-*/
-serial_port_err_t serialPortOpen(serial_port_t* port, const char* name, uint64_t baud, uint32_t readTimeout, uint32_t writeTimeout);
+
+serial_port_err_t setTimeouts(serial_port_t* port, uint64_t readTimeout, uint64_t writeTimeout)
+{
+    /* create a COMMTIMEOUTS structure and set the timeout values */
+    COMMTIMEOUTS timeouts;
+    timeouts.ReadIntervalTimeout = 0;           /* No timeout between subsequent reads */
+    timeouts.ReadTotalTimeoutConstant = readTimeout;
+    timeouts.ReadTotalTimeoutMultiplier = 0;
+    timeouts.WriteTotalTimeoutConstant = writeTimeout;
+    timeouts.WriteTotalTimeoutMultiplier = 0;
+
+    /* store the timeout values in the serial port handle */
+    port->readTimeout = readTimeout;
+    port->writeTimeout = writeTimeout;
+
+    /* set the timeouts and check for error */
+    if (!SetCommTimeouts(port->handle, &timeouts)) 
+        return SERIAL_ERR_UNKNOWN;
+    
+    /* return OK */
+    return SERIAL_ERR_OK;
+}
+
+
+serial_port_err_t setBaud(serial_port_t* port, uint64_t baudRate)
+{
+    /* create a DCB structure and set the Baudrate */
+    DCB dcb = {0};
+
+    /* set the size of DCB length to the size of the structure itself */
+    dcb.DCBlength = sizeof(DCB);
+    
+    /* get the current DCB state */
+    if (!GetCommState(port->handle, &dcb))
+        return SERIAL_ERR_UNKNOWN;
+
+    dcb.BaudRate = baudRate;
+    port->baud = baudRate;
+
+    /* set the new DCB values for the serial port */
+    if (!SetCommState(port->handle, &dcb))
+        return SERIAL_ERR_UNKNOWN;
+
+    /* return OK */
+    return SERIAL_ERR_OK;
+}
+
 
 serial_port_err_t serialPortOpen(serial_port_t* port, const char* name, uint64_t baud, uint32_t readTimeout, uint32_t writeTimeout) 
 {
@@ -87,6 +81,11 @@ serial_port_err_t serialPortOpen(serial_port_t* port, const char* name, uint64_t
     port->readTimeout = readTimeout;
     port->writeTimeout = writeTimeout;
 
+    /* set the baud rate and the timeouts */
+    setBaud(&port, baud);
+    setTimeouts(&port, readTimeout, writeTimeout);
+    
+
     /* open the serial port by opening it as a file with the following attributes */
     port->handle = CreateFileA(port->name, FILE_RW_MODE, FILE_NO_SHARED_ACCESS, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
     
@@ -104,18 +103,6 @@ serial_port_err_t serialPortOpen(serial_port_t* port, const char* name, uint64_t
     return SERIAL_ERR_OK;
 }
 
-/** 
- * Objective: To close the serial port instance and print 
- * any error if occured.
- * 
- * Parameters: 
- * serial_port_t* port: The serial port structure to close.
- * 
- * Returns: serial_port_err_t
- * SERIAL_ERR_OK: on successfully closing the serial port.
- * SERIAL_ERR_CLOSE: error occured while closing the serial port.
-*/
-serial_port_err_t serialPortClose(serial_port_t* port);
 
 serial_port_err_t serialPortClose(serial_port_t* port)
 {
@@ -134,20 +121,6 @@ serial_port_err_t serialPortClose(serial_port_t* port)
     return SERIAL_ERR_CLOSE;
 }
 
-/** 
- * Objective: To read from the serial port
- * 
- * Parameters: 
- * serial_port_t* port: The serial port instance.
- * uint8_t *buf: a buffer to store the read data.
- * uint64_t size: nos of expected bytes to read.
- * 
- * Returns: serial_port_err_t
- * SERIAL_ERR_OK: on successfully reading the expected nos of bytes.
- * SERIAL_ERR_READ_UNKNOWN: on unkwon reason.
- * SERIAL_ERR_READ_SIZE_MISMATCH: nos of bytes read != expected size.
-*/
-serial_port_err_t serialPortRead(serial_port_t* port, uint8_t *buf, uint64_t size);
 
 serial_port_err_t serialPortRead(serial_port_t* port, uint8_t *buf, uint64_t size)
 {
@@ -171,21 +144,6 @@ serial_port_err_t serialPortRead(serial_port_t* port, uint8_t *buf, uint64_t siz
 }
 
 
-/** 
- * Objective: To write to the serial port
- * 
- * Parameters: 
- * serial_port_t* port: The serial port instance.
- * uint8_t *buf: a buffer containing the data to write.
- * uint64_t size: size of the buffer.
- * 
- * Returns: serial_port_err_t
- * SERIAL_ERR_OK: on successfully writing the full buffer.
- * SERIAL_ERR_WRITE_UNKNOWN: on unkwon reason.
- * SERIAL_ERR_WRITE_SIZE_MISMATCH: nos of bytes written != buffer size.
-*/
-serial_port_err_t serialPortWrite(serial_port_t* port, uint8_t *buf, uint64_t size);
-
 serial_port_err_t serialPortWrite(serial_port_t* port, uint8_t *buf, uint64_t size)
 {
     /* to store the actual bytes written */
@@ -206,7 +164,6 @@ serial_port_err_t serialPortWrite(serial_port_t* port, uint8_t *buf, uint64_t si
     /* return OK */
     return SERIAL_ERR_OK;
 }
-
 
 
 int main()
---
+++ serialPort.h
@@ -0,0 +1,130 @@
+#include <errno.h>
+#include <stdint.h>
+#include <windows.h>
+
+#define FILE_NO_SHARED_ACCESS   0
+#define FILE_RW_MODE            (FILE_GENERIC_READ | FILE_GENERIC_WRITE)
+#define MAX_ERR_CODE_LEN        64u
+
+/* The Serial Port Structure to store the fields of the serial COMM */
+typedef struct {
+    HANDLE handle;          /* Handle to store the serial port as a file handle */
+    const char *name;       /* name of the serial COM port */
+    boolean isOpen;         /* Boolean value to indicate the port open status */
+    uint64_t baud;          /* Baud rate of the serial COM port */
+    uint32_t readTimeout;   /* read timeout in ms */
+    uint32_t writeTimeout;  /* write timeout in ms */
+}serial_port_t;
+
+/* error enum to store the error types */
+typedef enum {
+    SERIAL_ERR_OK,
+    SERIAL_ERR_OPEN,
+    SERIAL_ERR_CLOSE,
+    SERIAL_ERR_UNKNOWN,
+    SERIAL_ERR_READ_UNKNOWN,
+    SERIAL_ERR_READ_SIZE_MISMATCH,
+    SERIAL_ERR_WRITE_UNKNOWN,
+    SERIAL_ERR_WRITE_SIZE_MISMATCH,
+}serial_port_err_t;
+
+
+
+
+/** 
+ * Objective: To open the serial port and initialise the 
+ * the serial port handle.
+ * 
+ * Parameters: 
+ * serial_port_t* port: Pointer to the serial port structure.
+ * const char* name: String containing the name of the serial port.
+ * uint64_t baud: Baudrate of the serial port.
+ * uint32_t readTimeout: timeout in ms before read action returns.
+ * uint32_t writeTimeout: timeout in ms before write action returns.
+ *  
+ * Returns: serial_port_err_t
+ * SERIAL_ERR_OK: on successful poening the serial port.
+ * SERIAL_ERR_OPEN: could not open serial port.
+*/
+serial_port_err_t serialPortOpen(serial_port_t* port, const char* name, uint64_t baud, uint32_t readTimeout, uint32_t writeTimeout);
+
+/** 
+ * Objective: To close the serial port instance and print 
+ * any error if occured.
+ * 
+ * Parameters: 
+ * serial_port_t* port: The serial port structure to close.
+ * 
+ * Returns: serial_port_err_t
+ * SERIAL_ERR_OK: on successfully closing the serial port.
+ * SERIAL_ERR_CLOSE: error occured while closing the serial port.
+*/
+serial_port_err_t serialPortClose(serial_port_t* port);
+
+/** 
+ * Objective: To read from the serial port
+ * 
+ * Parameters: 
+ * serial_port_t* port: The serial port instance.
+ * uint8_t *buf: a buffer to store the read data.
+ * uint64_t size: nos of expected bytes to read.
+ * 
+ * Returns: serial_port_err_t
+ * SERIAL_ERR_OK: on successfully reading the expected nos of bytes.
+ * SERIAL_ERR_READ_UNKNOWN: on unkwon reason.
+ * SERIAL_ERR_READ_SIZE_MISMATCH: nos of bytes read != expected size.
+*/
+serial_port_err_t serialPortRead(serial_port_t* port, uint8_t *buf, uint64_t size);
+
+/** 
+ * Objective: To write to the serial port
+ * 
+ * Parameters: 
+ * serial_port_t* port: The serial port instance.
+ * uint8_t *buf: a buffer containing the data to write.
+ * uint64_t size: size of the buffer.
+ * 
+ * Returns: serial_port_err_t
+ * SERIAL_ERR_OK: on successfully writing the full buffer.
+ * SERIAL_ERR_WRITE_UNKNOWN: on unkwon reason.
+ * SERIAL_ERR_WRITE_SIZE_MISMATCH: nos of bytes written != buffer size.
+*/
+serial_port_err_t serialPortWrite(serial_port_t* port, uint8_t *buf, uint64_t size);
+
+/** 
+ * Objective: To set baud rate of the serial port
+ * 
+ * Parameters: 
+ * serial_port_t* port: The serial port instance.
+ * uint64_t baudRate: the Baud Rate
+ * 
+ * Returns: serial_port_err_t
+ * SERIAL_ERR_OK: on successful setting the Baud Rate.
+ * SERIAL_ERR_UNKNOWN: on unkwon reason.
+*/
+serial_port_err_t setBaud(serial_port_t* port, uint64_t baudRate);
+
+/** 
+ * Objective: To set read and write timeouts of the serial port
+ * 
+ * Parameters: 
+ * serial_port_t* port: The serial port instance.
+ * uint64_t readTimeout: Read timeout in ms
+ * uint64_t writeTimeout: Read timeout in ms
+ * 
+ * Returns: serial_port_err_t
+ * SERIAL_ERR_OK: on successful changing the timeout.
+ * SERIAL_ERR_UNKNOWN: on unkwon reason.
+*/
+serial_port_err_t setTimeouts(serial_port_t* port, uint64_t readTimeout, uint64_t writeTimeout);
+
+/** 
+ * Objective: To get the system error code and 
+ * print it's text format for human readability.
+ * 
+ * Parameters: NONE
+ * 
+ * Returns: NULL
+*/
+void printError();
+
